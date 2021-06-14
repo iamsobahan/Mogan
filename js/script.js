24aa737b9c52b17430c7126d7798bc85fff07ef1
sobahan-webhunter <sobahan.webhunter@gmail.com>
2021-06-14 20:46:51 +0600

    late upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.about-slider').slick({
+  dots: true,
+  infinite: Infinity,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    prevArrow: '<i class="fas fa-long-arrow-alt-left prev"></i>',
+    nextArrow: '<i class="fas fa-long-arrow-alt-right next"></i>',
+    
+    
+
+});
+
+
+
+var mixer = mixitup('.mother');
+
+
+
+$('.sliders').slick({
+  dots: true,
+  infinite: Infinity,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    prevArrow: '<i class="fas fa-long-arrow-alt-left pre"></i>',
+    nextArrow: '<i class="fas fa-long-arrow-alt-right nex"></i>',
+    
+    
+
+});
